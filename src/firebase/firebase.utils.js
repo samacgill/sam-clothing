a51c72aaf9c12b1b0984b68a0b21d94d4b0dd278
samacgill <samacgill@yahoo.co.uk>
2021-08-02 15:34:49 +0100

    set up firebase utils, including ability to store authenticated users in firestore database

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,6 +13,42 @@ const config = {
   measurementId: "G-BPV82LR26Y",
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+  // let userRef = firestore.collection("users").doc("DmergPQKGGKj7Wo88cxc");
+  // const userRef = firestore.doc("users/DmergPQKGGKj7Wo88cxc");
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  // const collRef = firestore.collection("users");
+  // console.log(collRef);
+
+  // the documentRef methods are .set(), .get(), .update() and .delete()
+  // can also add to collection using collectionRef object and .add() method
+  //documentRef.get() returns a documentSnapshot object
+  //collectionRef.get() returns a querySnapshot object
+
+  const snapShot = await userRef.get();
+  // console.log(snapShot);
+  // getting userRef creates new 'exists' field, which is false if user not there. so we create.
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log("error creating user", error.message);
+    }
+  }
+  // function returns userRef, which we can use to set state
+  return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
