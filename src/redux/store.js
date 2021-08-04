84daf00805280e2974bee1c395f0eda44fd27c03
samacgill <samacgill@yahoo.co.uk>
2021-08-04 22:06:15 +0100

    removing redux-logger from production build

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,11 @@ import logger from "redux-logger";
 
 import rootReducer from "./root-reducer";
 
-const middlewares = [logger];
+const middlewares = [];
+//could just have logger in middlewares, but this stops loggin in heroku (in production)
+if (process.env.NODE_ENV === "development") {
+  middlewares.push(logger);
+}
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
